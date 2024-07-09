6e5ee3d77ad4d72ff0a5c5d738866b70cae38037
papun kumar <papunkumar101>
2024-07-09 23:30:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world');
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
